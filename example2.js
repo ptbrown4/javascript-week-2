9bb637963ce85c2e48b67e7d9726fbe5961923e8
Terry Brown <ptbrown514@gmail.com>
2018-10-13 11:33:45 -0500

    truthly falsy

---
+++ example2.js
@@ -0,0 +1,25 @@
+
+function makeMessage(test){
+    if(test){
+        console.log("I called a function");
+    } else{
+    console.log("I called a function differently");
+    }
+}
+
+makeMessage(true);
+makeMessage(false);
+makeMessage(true);
+makeMessage(false);
+makeMessage(true);
+makeMessage(false);
+
+
+// loops inside of loops
+// for(var num1 = 1; num1 <= 9; num1++){
+//     message = ''
+//     for(var num2 = 1; num2 <= num1; num2++){
+//         message = message + num2 + " ";
+//     }
+//     console.log(message)
+// }
